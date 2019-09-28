ef6c55967dd56d7400eb761c8cffde52c30ba110
Cael Krigbaum <ckrigbaum@ConsolidatedFCU.local>
2019-09-27 17:14:07 -0700

    initial rewrite as of 09-27-2018.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = new express();
+
+const PORT = process.env.PORT || 8080;
+
+//set static folder
+app.use('/scripts',express.static(path.join(__dirname,'node_modules')));
+app.use('/content',express.static(path.join(__dirname,'content')));
+app.use('/css',express.static(path.join(__dirname,'public','css')));
+app.use(express.static(path.join(__dirname,'public')));
+
+app.listen(PORT,() => {console.log(`Server running on port ${PORT}`)});
