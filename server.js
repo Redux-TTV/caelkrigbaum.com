71770914a02291fad10e68667f95592784ae4cd6
Cael Krigbaum <krigbaum.cael@gmail.com>
2020-01-03 13:05:44 -0800

    Made the nav bar look a little nicer. added some new classes to facilitate the embossed look. added some eased-in underlines on nav items.

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 app.use('/scripts',express.static(path.join(__dirname,'node_modules')));
 app.use('/content',express.static(path.join(__dirname,'content')));
 app.use('/css',express.static(path.join(__dirname,'public','css')));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(__dirname));
+//app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(PORT,() => {console.log(`Server running on port ${PORT}`)});
